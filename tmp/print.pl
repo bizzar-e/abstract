9234ec9b574120d9c2121f4cb9eace1f58268998
Pavel Marakhovsky <paha_m@ukr.net>
2015-02-13 16:07:39 +0200

    Add to PL-121

---
+++ tmp/print.pl
@@ -0,0 +1,33 @@
+# Тестовый скрипт для построения плагинов саблайма
+
+# v1
+# print "bBB";
+
+# # v2
+# # C:\as_site\sym2\script\
+# open IN, "c:/as_site/sym2/script/test_py.py" or die $!;
+# undef $/; $in_code = <IN>; close IN;
+
+
+# # Файл для пересобранного вывода
+# open DEBUG, ">", "c:/debug_f5.txt" or die $!;
+
+# # Пересобранный вывод
+# print DEBUG "c:/AAA.txt", "\n";
+
+# print "Done\n";
+
+# close DEBUG;
+
+# v3
+# Эксперименты с автокомплитом в саблайме
+
+# print "FFXA\tXA FFXA_", "\n";
+# print "FFbA\tFb FFbA_";
+
+# v4, просто тесты
+
+$sel_name
+
+if ($sel_name =~ /$prefix/) {
+}
